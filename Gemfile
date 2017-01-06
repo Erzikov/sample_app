6f7915439477da3d99705b4871f39254d11859c0
Erzikov <lesha-erzikov@mail.ru>
2017-01-07 00:25:05 +0300

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ group :test do
 end
 
 
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
+gem 'faker', '1.1.2'
 gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sass-rails', '4.0.3'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do 
 	factory :user do 
-		name					"Test User"
-		email					"test-user@lol.com"
-		password				"123456"
-		password_confirmation	"123456"
+		sequence(:name)		{ |n| "Person #{n}" }
+		sequence(:email)	{ |n| "person_#{n}@email.com" }
+		password "password"
+		password_confirmation "password"
+
+		factory :admin do 
+			admin true
+		end
 	end
 end
